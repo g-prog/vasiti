c1949a16367a0675a43881520b7cf0f51f3af286
g-prog <alaobukky2@gmail.com>
2021-06-09 04:06:30 -0700

    newest changes to vasit

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,8 +3,58 @@ import "./portfolio.scss"
 export default function portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            <div className="left"></div>
-            <div className="right"></div>
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="assets/woman.png" alt="woman"/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <div className="content">
+                        <h1>Tolu & Joy's Experience</h1>
+                        <div className="button">
+                            <button>Customer</button>
+
+                        </div>
+                        
+                       <div className="text">
+                                <p>
+                                    I had the best experience shopping with vasiti.
+                                    
+                                </p>
+                                <p>Usability of the website was great, very good customer service and all-round great experience.
+                                    I would definitely be coming back!
+                                </p>
+                                <p>
+                                    I had the best experience shopping with vasiti.
+                                    
+                                </p>
+                                <p>Usability of the website was great, very good customer service and all-round great experience.
+                                    I would definitely be coming back!
+                                </p>
+                                <p>
+                                    I had the best experience shopping with vasiti.
+                                    
+                                </p>
+                                <p>Usability of the website was great, very good customer service and all-round great experience.
+                                    I would definitely be coming back!
+                                </p>
+                                <div className="share">
+                                    <h3>SHARE YOUR OWN STORY</h3>
+                                    <hr></hr>
+
+                                </div>
+
+                               
+                                
+                       </div>
+
+                    </div>
+                    
+                </div>
+            </div>
+
+           
             
             
         </div>
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,6 +3,97 @@ import "./works.scss"
 export default function Works() {
     return (
         <div className="works" id="works">
+            <div className="cards">
+                <div className="card1">
+                    <div className="image">
+                        <img src="assets/random.jpg" alt="cartoon"/>
+
+                    </div>
+                    <div className="content">
+                        <h3>Joseph Ike</h3>
+                        <div className="location">
+                            <div className="left">
+                                <p>In Ikeja</p>
+
+                            </div>
+                           
+                            <div className="button">
+                                <button><p>CUSTOMER</p></button>
+                            </div>
+                            
+                            
+
+                        </div>
+                        <div className="words">
+                            <p>blah blah blah blah blah</p>
+                            <p>blah blah blah blah blah</p>
+                            <p>blah blah blah blah blah</p>
+                            
+                        </div>
+                       
+                    </div>
+                </div>
+                <div className="card2">
+                    <div className="image">
+                        <img src="assets/random.jpg" alt="cartoon"/>
+
+                    </div>
+                    <div className="content">
+                        <h3>Joseph Ike</h3>
+                        <div className="location">
+                            <div className="left">
+                                <p>In Ikeja</p>
+
+                            </div>
+                           
+                            <div className="button">
+                                <button><p>CUSTOMER</p></button>
+                            </div>
+                            
+                            
+
+                        </div>
+                        <div className="words">
+                            <p>blah blah blah blah blah</p>
+                            <p>blah blah blah blah blah</p>
+                            <p>blah blah blah blah blah</p>
+                            <p>blah blah blah blah blah</p>
+                        </div>
+                       
+                    </div>
+                    
+                </div>
+                <div className="card3">
+                <div className="image">
+                        <img src="assets/random.jpg" alt="cartoon"/>
+
+                    </div>
+                    <div className="content">
+                        <h3>Joseph Ike</h3>
+                        <div className="location">
+                            <div className="left">
+                                <p>Ikeja</p>
+
+                            </div>
+                           
+                            <div className="button">
+                                <button><p>CUSTOMER</p></button>
+                            </div>
+                            
+                            
+
+                        </div>
+                        <div className="words">
+                            <p>blah blah blah blah blah</p>
+                            <p>blah blah blah blah blah</p>
+                            <p>blah blah blah blah blah</p>
+                        </div>
+                       
+                    </div>
+
+                </div>
+
+            </div>
             
             
         </div>
