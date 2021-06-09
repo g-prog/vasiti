15b48432e91b93b797548a530306f5184a29264c
g-prog <alaobukky2@gmail.com>
2021-06-09 00:51:38 -0700

    latest changes

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,6 +3,8 @@ import "./portfolio.scss"
 export default function portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <div className="left"></div>
+            <div className="right"></div>
             
             
         </div>
