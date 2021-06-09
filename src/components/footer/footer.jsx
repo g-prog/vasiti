b2cfd75acba6cfb54c62e3dd9117bd9d4713eabb
g-prog <alaobukky2@gmail.com>
2021-06-09 07:53:57 -0700

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,6 +3,14 @@ import "./footer.scss"
 export default function footer() {
     return (
         <div className="footer">
+            <div className="left">
+                <img src="assets/phone.png" alt="phone"/>
+
+            </div>
+            <div className="right">
+                <h1>Be a member of our community</h1>
+
+            </div>
             
         </div>
     )
